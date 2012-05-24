211ce10e455ace0425e11785714c554500534c33
David Rosen <david@wolfire.com>
2012-05-24 13:38:38 -0700

    Added <string> include

--- casecorrectpath.h
+++ casecorrectpath.h
@@ -2,6 +2,8 @@
 #ifndef CASE_CORRECT_PATH_H
 #define CASE_CORRECT_PATH_H
 
+#include <string>
+
 // Takes an input_path that is known to exist // e.g. "DaTa/fILE.zIP"
 // Fills correct_case with the true file path case // e.g. "Data/File.zip"
 // Returns whether or not input_path == correct_case
